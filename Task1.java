d1a31c86a3fc5c2b838f9c8d72314c3bf14e66ac
harshg2961 <118956982+harshg2961@users.noreply.github.com>
2023-01-16 10:50:45 +0530

    Add files via upload

---
+++ Task1.java
@@ -0,0 +1,38 @@
+public class Task1 {
+
+    public static void main(String[] args) {f
+
+        Book b = new Book();
+        Book b1 = new Book(10, "Java", "harsh");
+        b1.print();
+
+    }
+}
+
+class Book {
+
+    int book_id;
+    String title, author;
+
+    Book() {
+
+        System.out.println("Default");
+    }
+
+    Book(int b, String t, String a) {
+
+        book_id = b;
+        title = t;
+        author = a;
+
+    }
+
+    void print() {
+
+        System.out.println("Book Id = " + book_id);
+        System.out.println("Title = " + title);
+        System.out.println("Author = " + author);
+
+    }
+
+}
